6348db7698eb2362b06d53de8814a9c46f98f6f9
UppalaGoutham <98812442+UppalaGoutham@users.noreply.github.com>
2022-03-07 18:42:41 +0530

    Add files via upload

---
+++ Project/3_Implementation/timer/timer.h
@@ -0,0 +1,20 @@
+/**
+ * @file timer.h
+ * @author goutham
+ * @brief 
+ * @date 2022-03-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include "timer_config.h"
+
+
+
+void timer0_init(void);
+
+#endif
